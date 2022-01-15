9f270239277c9bde50c62832ec10562dc8556cc7
linyuanlong <3004208009@qq.com>
2022-01-15 17:14:37 +0800

    update upload picture

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,58 +4,56 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PublicController extends Controller {
-
     public function uploadImg(Request $request) {
         $data = ['state'=>'fail', 'msg'=>'上传失败', 'url'=>'','title' => '','original','type' => '','size' => 0];
-        if($request->hasFile('upfile') && $request->file('upfile')->isValid()) {
-            $maxSize = 1024 * 1024;
+        if ($request->hasFile('upfile')) {
+            $picture = $request->file('upfile');
+            if (!$picture->isValid()) {
+                $data['msg'] = '无效的上传文件';
+                return response()->json($data);
+            }
+            // 文件扩展名
+            $ext = $picture->extension();
             //支持的上传图片类型
             $allowed_extensions = ["png", "jpg", "gif","jpeg"];
-            $file = $request->file('upfile');
-
-            $ext = $file->extension();
-
             if (!in_array(strtolower($ext),$allowed_extensions)){
                 $data['msg'] = "请上传".implode(",",$allowed_extensions)."格式的图片";
                 $data['type'] = $ext;
                 return response()->json($data);
             }
-            // 检测大小
-            $fileSize = $file->getSize();
-            if($fileSize > $maxSize) {
-                $data['msg'] = "图片大小限制1M，你的图片大小为 " . round($fileSize/$maxSize,2) . 'M';
-                $data['size'] = round($fileSize/$maxSize,2);
+            // 生成新的统一格式的文件名
+            $newFileName = date('YmdHis',time()) . mt_rand(1, 9999999) . '.' . $ext;
+
+            // 日期目录
+            $date_dir = date('Ymd',time());
+            // 保存目录
+            $storePublicDir = 'images/' . $date_dir;
+            // 图片保存路径
+            $savePath = 'images/' . $date_dir . '/' . $newFileName;
+            // Web 访问路径
+            $webPath = '/storage/' . $savePath;
+            // 将文件保存到本地 storage/app/public/images 目录下，先判断同名文件是否已经存在
+            if (Storage::disk('public')->has($savePath)) {
+                $data['msg'] = '网络异常,上传失败，请重试!';
                 return response()->json($data);
             }
-            $date_dir = date('Ymd',time());
-            $image_path = 'public/images/' . $date_dir;
-            $new_file_name = date('YmdHis') . date('YmdHis',time()). rand(1,9999999) . '.' . $ext;
-            $store_result = $file->storeAs($image_path,$new_file_name);
-            // 新建公共目录
-
-            if($store_result){
-                // 项目所在的根目录
-                $root_dir = dirname(dirname(dirname(__DIR__)));
-                // 图片存在的目录
-                $pic_dir = $root_dir . '/public/images/' . $date_dir . '/';
-                // 存放的相对路径
-                $relative_path = 'images/' . $date_dir . '/' . $new_file_name;
-                $this->checkAndCreateDir($pic_dir);
-                $upload_file_ = $root_dir . '/storage/app/' . $image_path . '/' . $new_file_name;
-                $copy_file = $pic_dir . $new_file_name;
-                copy($upload_file_,$copy_file);
+            // 否则执行保存操作，保存成功将访问路径返回给调用方
+            if ($picture->storePubliclyAs($storePublicDir, $newFileName, ['disk' => 'public'])) {
                 $data['state'] = 'SUCCESS';
-                $data['sing_url'] = $relative_path;
-                $data['url'] = '/' . $relative_path;
+                $data['sing_url'] = substr($webPath,1);
+                $data['url'] = $webPath;
                 $data['msg'] = "上传成功";
                 return response()->json($data);
             }
+            $data['msg'] = '文件上传失败';
+            return response()->json($data);
+        } else {
+            $data['msg'] = '请选择要上传的文件';
             return response()->json($data);
         }
-        $data['msg'] = "未获取到上传文件或上传过程出错";
-        return response()->json($data);
 
     }
 
